30f13642cae2f0062008734d54782b2ea166ca4c
zheruiliu <jerryliu.me@gmail.com>
2015-05-28 00:51:49 -0500

    index page layout

---
+++ js/layout.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#locationDropDown').find('li').not('.disabled').click(function(){
+		var location = $(this).text();
+		$('#selectLocation').text(" " + location);
+	});
+	$('#locationDropDown').find('li').not('.disabled')[0].click();
+	
+	
+});
